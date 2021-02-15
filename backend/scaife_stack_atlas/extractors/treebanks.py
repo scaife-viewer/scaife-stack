85c860fedc34edba011a6a8118ba5697f468c35e
Jacob Wegner <jwegner@eldarion.com>
2021-02-15 12:49:44 -0600

    Add extractors and metadata to VCS
    The commands below were used to create the library data and syntax tree annotations
    from the Crito treebank:
    
    1) Change `TREEBANK_PATH` to the equivalent local path
    2) Run the following commands:
    
    ```
    python manage.py shell -c "from scaife_stack_atlas.extractors.versions import create_version; create_version();"
    python manage.py shell -c "from scaife_stack_atlas.extractors.treebanks import create_syntax_tree_annotation; create_syntax_tree_annotation();"
    ```

---
+++ backend/scaife_stack_atlas/extractors/treebanks.py
@@ -0,0 +1,73 @@
+import json
+import os
+
+from django.conf import settings
+
+from lxml import etree
+
+
+CRITO_VERSION_PART = "tlg0059.tlg003.pedalion-tb"
+TREEBANK_PATH = "/Users/jwegner/Data/development/repos/perseids-publications/pedalion-trees/public/xml/crit.xml"
+
+
+def transform_headwords(words):
+    inbounds = set(w["id"] for w in words)
+    for word in words:
+        if word["head_id"] not in inbounds:
+            word["original_head_id"] = word["head_id"]
+            word["head_id"] = 0
+    return words
+
+
+def create_syntax_tree_annotation():
+    exemplar = "0059-003"
+    with open(TREEBANK_PATH) as f:
+        tree = etree.parse(f)
+    version = f"urn:cts:greekLit:{CRITO_VERSION_PART}:"
+    to_create = []
+    for sentence in tree.xpath("//sentence"):
+        sentence_id = sentence.attrib["id"]
+        sentence_obj = {
+            "urn": f'urn:cite2:pedalion:syntaxTree.v1:syntaxTree-{exemplar}-{sentence.attrib["id"]}',
+            "treebank_id": sentence_id,
+            "words": [],
+        }
+        for word in sentence.xpath(".//word"):
+            id_val = int(word.attrib["id"])
+            head_val = int(word.attrib["head"])
+
+            word_obj = {
+                "id": id_val,
+                "gloss": word.attrib.get("gloss", ""),
+                "value": word.attrib["form"],
+                "head_id": head_val,
+                "relation": word.attrib["relation"],
+                "lemma": word.attrib.get("lemma", ""),
+                "tag": word.attrib.get("postag", ""),
+            }
+
+            sentence_obj["words"].append(word_obj)
+
+        sentence_obj["words"] = transform_headwords(sentence_obj["words"])
+        # NOTE: We assume a 1:1 relationship between sentences and text parts at this time
+        ref = f'{sentence.attrib["subdoc"]}.{sentence.attrib["id"]}'
+        sentence_obj["references"] = [f"{version}{ref}"]
+
+        subdoc = sentence.attrib.get("subdoc")
+        if subdoc:
+            citation = f"{subdoc} ({sentence_id})"
+        else:
+            citation = f"({sentence_id})"
+        sentence_obj["citation"] = citation
+        to_create.append(sentence_obj)
+
+    version_part = version.rsplit(":")[-2]
+    output_path = os.path.join(
+        settings.SV_ATLAS_DATA_DIR,
+        "annotations",
+        "syntax-trees",
+        f"syntax_trees_{version_part}.json",
+    )
+    json.dump(
+        to_create, open(output_path, "w"), ensure_ascii=False, indent=2,
+    )
---
+++ backend/scaife_stack_atlas/extractors/versions.py
@@ -0,0 +1,57 @@
+import os
+
+from django.conf import settings
+
+from lxml import etree
+
+
+CRITO_VERSION_PART = "tlg0059.tlg003.pedalion-tb"
+TREEBANK_PATH = "/Users/jwegner/Data/development/repos/perseids-publications/pedalion-trees/public/xml/crit.xml"
+PUNCTUATION = "u--------"
+
+
+def aldt_to_textparts(path):
+    with open(path) as f:
+        tree = etree.parse(f)
+
+    text_parts = []
+    for sentence in tree.xpath("//sentence"):
+        words = []
+        prefix = None
+        for pos, word in enumerate(sentence.xpath(".//word")):
+            if word.attrib["postag"] == PUNCTUATION:
+                if pos == 0:
+                    prefix = f'{word.attrib["form"]}'
+                else:
+                    words[pos - 1] = f'{words[pos-1]}{word.attrib["form"]}'
+                words.append("")
+            else:
+                words.append(word.attrib["form"])
+        if prefix:
+            words[1] = f"{prefix}{words[1]}"
+        ref = f'{sentence.attrib["subdoc"]}.{sentence.attrib["id"]}'
+        text_parts.append((ref, " ".join(w for w in words if w)))
+    return text_parts
+
+
+def write_textparts(path, text_parts):
+    with open(path, "w") as f:
+        for text_part in text_parts:
+            f.write(" ".join(text_part))
+            f.write("\n")
+
+
+def create_version():
+    """
+    Extracts refs and text parts from the Crito treebank
+    """
+    text_parts = aldt_to_textparts(TREEBANK_PATH)
+    output_path = os.path.join(
+        settings.SV_ATLAS_DATA_DIR,
+        "library",
+        "tlg0059",
+        "tlg003",
+        f"{CRITO_VERSION_PART}.txt",
+    )
+
+    write_textparts(output_path, text_parts)
